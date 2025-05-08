ebaa266ae5bc44b5b19461d1d52577d9f9d0df69
Casper Zanichell <casperzanichelli98@gmail.com>
2025-05-08 11:52:50 +0200

    todays lecture

---
+++ code-alongs/database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE DEMO_DB;
+
+SHOW DATABASES;
+
+
+CREATE SCHEMA DEMO_DB.STAGING;
+
+CREATE TABLE customer(
+    customer_id integer PRIMARY KEY,
+    age integer,
+    email varchar(50)
+);
